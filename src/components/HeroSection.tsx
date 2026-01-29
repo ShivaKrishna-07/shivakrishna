dc293b7a3f72643a0c8a7e08a79260f5d55f4994
Shiva Krishna <135131547+ShivaKrishna-07@users.noreply.github.com>
2026-01-29 20:16:16 +0530

    Refactor HeroSection component and update social links

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -104,7 +104,7 @@ export default function HeroSection() {
             <FadeUp key="resume-button" duration={0.6} delay={0.6}>
               <div className="pointer-events-auto mt-8 flex justify-center">
                 <a
-                  href="https://drive.google.com/file/d/1Ad7odhnNnEov3jwBrD5Nvf_lFRO382rO/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1xuvxZX2toPEBOXU4u6h2dsuwvlSx0MJ_/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="bg-[#26a0a6] hover:bg-teal-600 text-white dark:text-black  px-6 py-2 rounded-full font-bold transition-colors"
@@ -119,6 +119,7 @@ export default function HeroSection() {
     </motion.section>
   );
 }
+
 
 
 
